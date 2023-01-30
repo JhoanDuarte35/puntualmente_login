3f4ad16146523cbe5963313b5e1cc8ead33e211e
JhoanDuarte35 <81330498+JhoanDuarte35@users.noreply.github.com>
2023-01-29 20:15:39 -0500

    all app

---
+++ app/views/admins/admin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+</head>
+<body>
+    <h1>Hola admin</h1>
+    <!-- <a href="http://localhost/puntualmente_login/index.php?action=operarios">Intefaz operarios</a> -->
+    <a href="http://localhost/puntualmente_login/index.php?action=cerrar">cerrar sesión</a>
+    
+    
+</body>
+</html>
